3d05cabd1960000f7f8a2161ec0c0f855f8bf7f2
Tarak.Mahmud <tarak.mahmud@northsouth.edu>
2020-07-21 10:33:58 -0700

    Assesment2.2 commit

---
+++ Lab Asssessment Submission/Lab Assessment 2/Assesment2.2.asm
@@ -0,0 +1,18 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+;a DB 10h,15h,10h,11h,12h ;
+;b DB 10 DUP <?>
+
+;c DB DUP <4,2,6,0,4,0,0,2,0,8>;
+
+
+
+ret
+
+
+
+
